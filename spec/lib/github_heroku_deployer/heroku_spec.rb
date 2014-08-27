749611df38eae289949a0c8a09d129bd941c507d
Michael Mitchell <michael.mitchell@getg5.com>
2014-08-27 15:00:13 -0700

    fixes specs, adds env vars required for tests

--- spec/lib/github_heroku_deployer/heroku_spec.rb
+++ spec/lib/github_heroku_deployer/heroku_spec.rb
@@ -7,7 +7,8 @@ describe GithubHerokuDeployer::Heroku do
       heroku = GithubHerokuDeployer::Heroku.new(
         heroku_api_key: "asdfghjkl",
         heroku_app_name: "asdfghjkl")
-      heroku.stub("find_app").and_raise(::Heroku::API::Errors::NotFound.new("", ""))
+      response = mock(body: 'error')
+      heroku.stub("find_app").and_raise(::Heroku::API::Errors::NotFound.new('',response))
       heroku.stub("create_app").and_return true
       heroku.find_or_create_app.should be true
     end
