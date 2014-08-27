2487a1e11225139948913dda829388a353dbc644
Michael Mitchell <michael.mitchell@getg5.com>
2014-08-27 16:51:20 -0700

    goes through platform_api to create an app that doesnt exist, in order to leverage its ability to create Heroku Organization apps

--- spec/lib/github_heroku_deployer/heroku_spec.rb
+++ spec/lib/github_heroku_deployer/heroku_spec.rb
@@ -1,16 +1,29 @@
 require 'spec_helper'
 require 'github_heroku_deployer/heroku'
+require 'platform-api'
 
 describe GithubHerokuDeployer::Heroku do
   context "#find_or_create" do
+    before do
+      PlatformAPI.stub(:connect_oauth)
+    end
+
     it "creates app if it does not find it" do
       heroku = GithubHerokuDeployer::Heroku.new(
+        heroku_organization_name: "test-org",
         heroku_api_key: "asdfghjkl",
         heroku_app_name: "asdfghjkl")
       response = mock(body: 'error')
       heroku.stub("find_app").and_raise(::Heroku::API::Errors::NotFound.new('',response))
-      heroku.stub("create_app").and_return true
-      heroku.find_or_create_app.should be true
+
+      organization_app = double(:organization_app, create: "created")
+      platform_api = double(:mock, :organization_app => organization_app)
+      PlatformAPI.stub(:connect_oauth).and_return(platform_api)
+
+      organization_app.should_receive(:create).with(hash_including(organization: "test-org"))
+
+      heroku.find_or_create_app
     end
   end
 end
+
