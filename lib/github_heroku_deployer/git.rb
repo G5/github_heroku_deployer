a78da5134839a12c9b9ba2392df4d107ff6343c0
Chris Eckert​ <christopher.k.eckert@gmail.com>
2015-04-12 12:44:10 -0700

    WIP: Intitializer refactoring

--- lib/github_heroku_deployer/git.rb
+++ lib/github_heroku_deployer/git.rb
@@ -10,36 +10,38 @@ module GithubHerokuDeployer
       @id_rsa = options[:id_rsa]
       @logger = options[:logger]
       @repo_dir = options[:repo_dir]
+      @folder = setup_folder
+      @repo = setup_repo
     end
 
     def push_app_to_heroku(remote="heroku", branch="master", &block)
       wrapper = ssh_wrapper
-      run "cd #{repo.dir}; git remote rm #{remote}" if repo.remote(remote).url
-      repo.add_remote(remote, @heroku_repo)
-      yield(repo) if block_given?
-      @logger.info "deploying #{repo.dir} to #{repo.remote(remote).url} from branch #{branch}"
-      run "cd #{repo.dir}; env #{wrapper.git_ssh} git push -f #{remote} #{branch}"
+      run "cd #{@repo.dir}; git remote rm #{remote}" if @repo.remote(remote).url
+      @repo.add_remote(remote, @heroku_repo)
+      yield(@repo) if block_given?
+      @logger.info "deploying #{@repo.dir} to #{@repo.remote(remote).url} from branch #{branch}"
+      run "cd #{@repo.dir}; env #{wrapper.git_ssh} git push -f #{remote} #{branch}"
     ensure
       wrapper.unlink
     end
 
-    def repo
-      @repo ||= setup_repo
-    end
+    # def repo
+    #   @repo ||= setup_repo
+    # end
 
     def setup_repo
       clone_or_pull
       open
     end
 
-    def folder
-      @folder ||= setup_folder
-    end
+    # def folder
+    #   @folder ||= setup_folder
+    # end
 
     def setup_folder
-      folder = File.join(@repo_dir, Zlib.crc32(@github_repo).to_s)
-      FileUtils.mkdir_p(folder)
-      folder
+      tmp_folder = File.join(@repo_dir, Zlib.crc32(@github_repo).to_s)
+      FileUtils.mkdir_p(tmp_folder)
+      tmp_folder
     end
 
     def clone_or_pull
@@ -47,13 +49,13 @@ module GithubHerokuDeployer
     end
 
     def exists_locally?
-      File.exists?(File.join(folder, ".git", "config"))
+      File.exists?(File.join(@folder, ".git", "config"))
     end
 
     def clone
       wrapper = ssh_wrapper
-      @logger.info "cloning #{@github_repo} to #{folder}"
-      run "env #{wrapper.git_ssh} git clone #{@github_repo} #{folder}"
+      @logger.info "cloning #{@github_repo} to #{@folder}"
+      run "env #{wrapper.git_ssh} git clone #{@github_repo} #{@folder}"
     ensure
       wrapper.unlink
     end
@@ -61,14 +63,14 @@ module GithubHerokuDeployer
     def pull
       wrapper = ssh_wrapper
       dir = Dir.pwd # need to cd back to here
-      @logger.info "pulling from #{folder}"
-      run "cd #{folder}; env #{wrapper.git_ssh} git pull; cd #{dir}"
+      @logger.info "pulling from #{@folder}"
+      run "cd #{@folder}; env #{wrapper.git_ssh} git pull; cd #{dir}"
     ensure
       wrapper.unlink
     end
 
     def open
-      ::Git.open(folder)
+      ::Git.open(@folder)
     end
 
     def ssh_wrapper
