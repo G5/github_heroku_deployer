98030b99ab130357543bf4c4d86a15f0d7e41bfc
Mark Bluhm <mark.bluhm@getg5.com>
2017-07-07 11:52:15 -0700

    revert branch default to master

--- lib/github_heroku_deployer/git.rb
+++ lib/github_heroku_deployer/git.rb
@@ -12,17 +12,16 @@ module GithubHerokuDeployer
       @repo_dir = options[:repo_dir]
     end
 
-    def push_app_to_heroku(remote="heroku", branch="update-gemfilelock", &block)
+    def push_app_to_heroku(remote="heroku", branch="master", &block)
       wrapper = ssh_wrapper
       run "cd #{repo.dir}; git remote rm #{remote}" if repo.remote(remote).url
       repo.add_remote(remote, @heroku_repo)
       yield(repo) if block_given?
       @logger.info "deploying #{repo.dir} to #{repo.remote(remote).url} from branch #{branch}"
-      run "cd #{repo.dir}; git checkout #{branch}; env #{wrapper.git_ssh} git push -f #{remote} #{branch}:master"
+      run "cd #{repo.dir}; env #{wrapper.git_ssh} git push -f #{remote} #{branch}"
     ensure
       wrapper.unlink
     end
-
 
     def repo
       @repo ||= setup_repo
